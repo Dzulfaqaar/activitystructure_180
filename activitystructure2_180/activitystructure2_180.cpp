5e206bfe05004928d729f5a9b38b1f699d30e63f
dzulfaqaar <fikri6aul@gmail.com>
2023-04-03 15:02:58 +0700

    input mahasiswa

--- activitystructure2_180/activitystructure2_180.cpp
+++ activitystructure2_180/activitystructure2_180.cpp
@@ -14,3 +14,24 @@ struct Mahasiswa {
 	int umur;
 
 };
+
+int main() {
+	Mahasiswa mhs;
+	
+	cout << "masukkan nim :";
+	cin >> mhs.nim;
+	cout << "masukkan nama :";
+	cin >> mhs.nim;
+	cout << " alamat :" << endl;
+	cout << "\ntnama desa :";
+	cin >> mhs.alamat.desa;
+	cout << "\ntnama kota :";
+	cin >> mhs.alamat.kota;
+	cout << "masukkan umur :";
+	cin >> mhs.umur;
+
+	cout << "\nnim :" << mhs.nim;
+	cout << "\nnama :" << mhs.nama;
+	cout << "\ndesa :" << mhs.alamat.desa;
+	cout << "\nkota :" << mhs.alamat.kota;
+}
